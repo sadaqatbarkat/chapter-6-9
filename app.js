7548285765089f6a6e9ad40c9c50e3191a2c6842
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-03-15 17:08:49 +0500

    add index and app.js files

---
+++ app.js
@@ -0,0 +1,292 @@
+//Q1
+// let a =10;
+// console.log(a)  
+
+
+// ++a ;
+// console.log(a);                           
+
+// a++;
+// console.log(a);  
+
+
+// --a;
+// console.log(a);   
+
+// a--;
+// console.log(a);   
+
+
+
+
+
+//Q2
+
+// let a=2;
+// let b =1;
+// var result= --a  - --b + ++b + b--;
+// console.log(result)
+
+
+
+
+//Q3
+// let name=prompt("Asslam-o-alikum What is Your name?")
+// console.log(name)
+
+
+//Q6 
+
+// let subject1 = prompt("Enter the first Subject")
+// let totalMarks = +prompt("Enter the total Marks");
+// let obtainedMarks1 = +prompt("Enter the 1st obtained marks")
+// let percentage1 = prompt("Enter the 1st percentage")
+
+// let subject2 = prompt("Enter the second Subject")
+// let obtainedMarks2 = +prompt("Enter the  2nd obtained marks")
+// let percentage2 = +prompt("Enter the 2nd percentage")
+
+// let subject3 = prompt("Enter the third Subject")
+// let obtainedMarks3 = +prompt("Enter the 3rd obtained marks")
+// let percentage3 = +prompt("Enter the 3rd percentage")
+
+// document.write(
+// 	"<table>"
+// 	+ "<tr>"
+// 	+ "<th>" + "Subject" + "</th>"
+// 	+ "<th>" + "TotalMarks" + "</th>"
+// 	+ "<th>" + "Obtained Marks" + "</th>"
+// 	+
+ //"<th>" + "Percentage" + "</th>"
+
+// 	+ "</tr>"
+
+// 	+ "<tr>"
+// 	+ "<td>" + subject1 + "</td>"
+// 	+ "<td>" + totalMarks + "</td>"
+// 	+ "<td>" + obtainedMarks1 + "</td>"
+// 	+ "<td>" + percentage1 + "</td>"
+
+
+// 	+ "<tr>"
+
+// 	+ "<tr>"
+// 	+ "<td>" + subject2 + "</td>"
+// 	+ "<td>" + totalMarks + "</td>"
+// 	+ "<td>" + obtainedMarks2 + "</td>"
+// 	+ "<td>" + percentage2 + "</td>"
+
+
+// 	+ "<tr>"
+
+
+// 	+ "<tr>"
+// 	+ "<td>" + subject3 + "</td>"
+// 	+ "<td>" + totalMarks + "</td>"
+// 	+ "<td>" + obtainedMarks3 + "</td>"
+// 	+ "<td>" + percentage3 + "</td>"
+
+
+// 	+ "<tr>"
+
+
+
+// 	+ "<tr>"
+// 	+ "<th>" + "</th>"
+// 	+ "<th>" + "300" + "</th>"
+// 	+ "<th>" + obtainedMarks1 + obtainedMarks2 + obtainedMarks3 + "</th>"
+// 	+ "<th>" + percentage1 + percentage2 + percentage3 + "</th>"
+
+
+// 	+ "<tr>"
+
+
+
+
+
+// 	+ "</table>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	"<table border=1px solid black>"
+// 	+ "<tr>"
+// 	+ "<th>" + " Subject" + "</th>"
+// 	+ "<th>" + "Total Marks" + "</th>"
+// 	+ "<th>" + "Obtained Marks" + "</th>"
+// 	+ "<th>" + "Percentage " + "</th>"
+// 	+ "</tr>"
+// 	+ "<tr>"
+
+// 	+ "<td>" + subject1 + "<td>"
+// 	+"<td>"  + totalMarks +"</td>"
+// 	+ "<td>" + obtainedMarks1 + "<td>"
+// 	+ "<td>" + percentage1 + "<td>"
+// 	+ "</tr>"
+
+// 	+"<tr>"
+// 	+"<td>" +  subject2    +"<td>"
+// 	+"<td>"  + totalMarks +"</td>"
+// 	+"<td>" +  obtainedMarks2    +"<td>"
+// 	+"<td>" + percentage2    +"<td>"
+// 	+"</tr>"
+
+// 	+"<tr>"
+// 	+"<td>" +  subject3    +"<td>"
+// 	+"<td>"  + totalMarks +"</td>"
+// 	+"<td>" +  obtainedMarks3    +"<td>"
+// 	+"<td>" + percentage3    +"<td>"
+// 	+"</tr>"
+
+
+
+
+
+// +"<tr>"
+// +"<th>"    +"</th>"
+// +"<th>" +"300"   +"</th>"
+// +"<th>" +   obtainedMarks1 + obtainedMarks2 + obtainedMarks3    +"</th>"
+// +"<th>" +   percentage1 + percentage2 + percentage3    +"</th>"
+
+
+
+
+// +"<tr>"
+
+// 	+ "</table>"
+
+
+
+
